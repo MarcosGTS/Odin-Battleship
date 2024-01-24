9a1fcbe85328c80aa6d1c92cb9ba719e29fdfecb
MarcosGTS <marcosgtsodre@gmail.com>
2024-01-24 19:08:25 -0300

    Add basic interface

---
+++ scripts/domManager.js
@@ -0,0 +1,56 @@
+import { Game } from "./game";
+
+const content = document.querySelector("#content");
+
+function loadPage(pageCreator) {
+    content.innerHTML = '';
+    content.appendChild(pageCreator());
+}
+
+function renderMenu() {
+    const menuHtml = document.createElement("div");
+    menuHtml.innerHTML = `
+        <h1>menu</h1>
+        <button id="to-game">game</button>
+    
+    `
+    menuHtml.querySelector('#to-game').addEventListener("click", () => loadPage(renderGame));
+
+    return menuHtml;
+}
+
+function renderOptions() {
+    const optionsHtml = document.createElement("div");
+    optionsHtml.innerHTML = `
+        <h1>Options</h1>
+        <button id="to-game">game</button>
+    `;
+
+    optionsHtml.querySelector("#to-game").addEventListener("click", () => loadPage(renderGame));
+    return optionsHtml; 
+}
+
+function renderGame() {
+    const gameHtml = document.createElement("div");
+    const game = Game();
+
+    gameHtml.innerHTML = `
+        <h1>Game</h1>
+        <div>
+            <canvas id="board1" class="board"></canvas>
+        </div>
+        <button id="to-menu">Menu</button>
+        <button id="to-options">Options</button>
+    `
+
+    const board = gameHtml.querySelector("#board1");
+    renderBoard(board, game.getPlayers()[0]);
+
+    gameHtml.querySelector("#to-menu").addEventListener("click", () => loadPage(renderMenu));
+    gameHtml.querySelector("#to-options").addEventListener("click", () => loadPage(renderOptions));
+
+    return gameHtml;
+}
+
+
+loadPage(renderMenu);
